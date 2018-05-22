14a287205a7e1381789454aa78e508ac764780ac
sfhsdfasg <GertMorten.Paimla@tptlive.ee>
2018-05-22 12:37:51 +0300

    zz

---
+++ App7/App7/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App7.ViewModels
+{
+	public class HomeViewModel : BaseViewModel
+	{
+		public HomeViewModel ()
+		{
+			Title = "Tallinna polütehnikum";
+		}
+	}
+}
---
+++ App7/App7/ViewModels/TimesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App7.ViewModels
+{
+	public class TimesViewModel : BaseViewModel
+	{
+		public TimesViewModel ()
+		{
+			Title = "Tunniajad";
+		}
+	}
+}
